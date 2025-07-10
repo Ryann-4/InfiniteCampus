0b55af6eda441911e6e81e74a0f7af31123a21b1
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-07-10 20:12:10 +0000

    images test

--- messages.js
+++ messages.js
@@ -17,18 +17,18 @@ async function fetchMessages() {
         ? `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.png`
         : `https://cdn.discordapp.com/embed/avatars/0.png`;
       const timestamp = new Date(msg.timestamp).toLocaleString();
-      const imageRegex = /(https?:\/\/[^\s]+\.(png|jpg|jpeg|gif|webp))/i;
-      const match = msg.content.match(imageRegex);
-      let imageHTML = '';
-      if (match) {
-        imageHTML = `<br><img class="message-img" src="${match[1]}" alt="image">`;
-      }
+
+      // Auto-wrap image URLs in <img> tags
+      const imageRegex = /(https?:\/\/[^\s]+?\.(png|jpe?g|gif|webp))/gi;
+      const formattedContent = msg.content.replace(imageRegex, (url) => {
+        return `<br><img class="message-img" src="${url}" alt="image">`;
+      });
+
       li.innerHTML = `
         <img src="${avatarUrl}" class="avatar">
         <div class="content">
           <strong>${msg.author.username}</strong>
-          <div>${msg.content}</div>
-          ${imageHTML}
+          <div>${formattedContent}</div>
           <div class="timestamp">${timestamp}</div>
         </div>`;
       list.prepend(li);
@@ -37,6 +37,7 @@ async function fetchMessages() {
     console.error('Error fetching messages:', err);
   }
 }
+
 
 async function sendMessage(name, content) {
   const channelId = getSelectedChannelId();
