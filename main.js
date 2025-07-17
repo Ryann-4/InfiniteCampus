6936670f7068b214342a3da77abad9445b379e4f
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-07-17 14:16:48 +0000

    removed useless button

--- main.js
+++ main.js
@@ -31,10 +31,6 @@ function converter() {
 function puter() {
     window.open("https://puter.com");
 }
-function exploit() {
-    alert('When You Type Stuff In You Will Still See It In Your History PS Type In The Box That Says "null"') 
-    window.open("https://sw.mzdy.com/!")
-}
 function thumbnail() {
     window.open("https://tagmp3.net/")
 }
