a2c7bbcaa0340b19b85f6e2b60d2ef0e7b4550b1
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-07-28 16:48:08 -0500

    test 8

--- drypopup.js
+++ drypopup.js
@@ -19,7 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
       </div>
       <div class="bar test">
         <center>
-          <a id="clocks" class="w headerbtn" href="InfiniteContacts" style="margin-top:-15px; margin-left:-50px; font-size:15px;">
+          <a class="bar headerbtn" href="InfiniteContacts" style="height:auto; width:auto; background:none; margin-top:-15px; margin-left:-50px; font-size:15px;">
             Contact Me
           </a>
         </center>
