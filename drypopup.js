3aa48cc4f88bea27460e83e2b6b25f40b750dc85
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-08-03 14:47:04 -0500

    fixed disabled 2

--- drypopup.js
+++ drypopup.js
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 <br>
                 <br>
                 <br>
-                <a class="button poll disabled" href="InfinitePolls">
+                <a class="button poll disabled">
                     Take A Quick Survey
                 </a>
                 <br>
