00f49b3e4c25a976a7e68aea1e70282d269ba3fe
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-08-13 12:40:15 -0500

    did stuff

---
+++ archive.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.vhtml').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const fileURL = this.getAttribute('href');
+        fetch(fileURL)
+            .then(response => response.text())
+            .then(html => {
+                const win = window.open();
+                win.document.write('<pre>' +
+                    html.replace(/</g, '&lt;').replace(/>/g, '&gt;') +
+                    '</pre>');
+            })
+            .catch(err => alert('Error: ' + err));
+    });
+});
